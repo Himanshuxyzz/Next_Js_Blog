b04336c0e51669ffcbe9f09a7dd96f40d48fcb4e
Himanshuxyzz <himanshuxyzz@gmail.com>
2023-08-19 15:41:34 +0530

    chores: setting up custom error 404 page✅

--- components/404/Error404.tsx
+++ components/404/Error404.tsx
@@ -7,7 +7,7 @@ const Error404 = ({ message }: {
   return (
     <div className='@container'>
       <div className='mb-10 mx-auto max-w-[640px] max-h-[180px]'>
-        <Image className='w-full object-cover ' priority alt='404 image' width={"640"} height={180} src={"/sukuna.png"} />
+        <Image className='w-full object-cover ' priority alt='404 image' width={"640"} height={"180"} src={"/sukuna.png"} />
       </div>
       <div className='mb-10 h-20 text-center '>
         <h2 className='font-semibold text-base @md:text-1xl @lg:text-2xl'>{message}</h2>
