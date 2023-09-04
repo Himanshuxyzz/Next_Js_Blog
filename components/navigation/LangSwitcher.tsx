c424d5d10fe6eaa1addfabc76f1e2387950bf782
Himanshuxyzz <himanshuxyzz@gmail.com>
2023-09-04 15:19:12 +0530

    chores: implementing redirect logic to make sure on changing locale doest reset page or to persist the page  ✅

--- components/navigation/LangSwitcher.tsx
+++ components/navigation/LangSwitcher.tsx
@@ -7,11 +7,24 @@ const LangSwitcher = ({ locale }: {
     locale: string;
 }) => {
     // console.log(locale)
-    const pathName = usePathname()
+
 
     const targetLanguage = locale === "en" ? "de" : "en";
+    const pathName = usePathname()
+
+    const redirectTarget = () => {
+        if (!pathName) return "/"
+        const segments = pathName.split("/");
+        // console.log(segments)
+        segments[1] = targetLanguage;
+        return segments.join("/")
+    }
+
     return (
-        <Link href={locale === "en" ? "/de" : "/en"}>{targetLanguage.toUpperCase()}</Link >
+        // <Link href={locale === "en" ? "/de" : "/en"}>{targetLanguage.toUpperCase()}</Link >
+        <Link href={redirectTarget()} locale={targetLanguage}>{targetLanguage.toUpperCase()}</Link >
+
+
     )
 }
 
